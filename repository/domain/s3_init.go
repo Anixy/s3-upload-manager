6ee48e23e36aa7b80317f8ce0aebf918e1b61d38
Muhammad Aniq Dhiyauddin <aniq.dhiyauddin@tokopedia.com>
2023-06-26 14:36:13 +0700

    Add domain s3 func & file service

--- repository/domain/s3_init.go
+++ repository/domain/s3_init.go
@@ -1,1 +1,25 @@
+// Package domain provides an interface for interacting with Amazon S3
 package domain
+
+import (
+	"io"
+
+	"github.com/minio/minio-go"
+)
+
+// S3 is an interface for interacting with Amazon S3
+type S3 interface {
+    UploadObject(objectName string, reader io.Reader, objectSize int64, opts minio.PutObjectOptions) (n int64, err error)
+}
+ // s3Impl is a concrete implementation of the S3 interface
+type s3Impl struct {
+    minio.Client
+	BucketName string
+}
+ // NewS3 initializes a new S3 implementation and returns it as an S3 interface
+func NewS3(minioClient minio.Client, bucketName string) S3 {
+    return &s3Impl{
+        Client: minioClient,
+		BucketName: bucketName,
+    }
+}
--- repository/domain/s3_method.go
+++ repository/domain/s3_method.go
@@ -1,1 +1,17 @@
 package domain
+
+import (
+	"io"
+	"log"
+
+	"github.com/minio/minio-go"
+)
+
+// UploadObject uploads an object to the specified S3 bucket with the given object name, reader, size and options.
+func (d *s3Impl) UploadObject(objectName string, reader io.Reader, objectSize int64, opts minio.PutObjectOptions) (n int64, err error) {
+	n, err = d.Client.PutObject(d.BucketName, objectName, reader, objectSize, opts)
+	if err != nil {
+		log.Println(err)
+	}
+	return
+}
