e2e5fb3e49133070854780beb632eaacb7a3e311
Muhammad Aniq Dhiyauddin <aniq.dhiyauddin@tokopedia.com>
2023-06-26 15:21:12 +0700

    ping endpoint & mod tidy

--- repository/domain/s3_init.go
+++ repository/domain/s3_init.go
@@ -9,17 +9,19 @@ import (
 
 // S3 is an interface for interacting with Amazon S3
 type S3 interface {
-    UploadObject(objectName string, reader io.Reader, objectSize int64, opts minio.PutObjectOptions) (n int64, err error)
+	UploadObject(objectName string, reader io.Reader, objectSize int64, opts minio.PutObjectOptions) (n int64, err error)
 }
- // s3Impl is a concrete implementation of the S3 interface
+
+// s3Impl is a concrete implementation of the S3 interface
 type s3Impl struct {
-    minio.Client
+	minio.Client
 	BucketName string
 }
- // NewS3 initializes a new S3 implementation and returns it as an S3 interface
+
+// NewS3 initializes a new S3 implementation and returns it as an S3 interface
 func NewS3(minioClient minio.Client, bucketName string) S3 {
-    return &s3Impl{
-        Client: minioClient,
+	return &s3Impl{
+		Client:     minioClient,
 		BucketName: bucketName,
-    }
+	}
 }
